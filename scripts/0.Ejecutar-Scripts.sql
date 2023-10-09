c0cdd3891ff5cabcffcf3de5cae418a63b8636fb
Carolina Suarez <danielasu8825@gmail.com>
2023-10-09 23:59:17 +0000

    comentarios

---
+++ scripts/0.Ejecutar-Scripts.sql
@@ -0,0 +1,35 @@
+-- Creación Bodega de Datos
+
+source /scripts/1.Crear-BodegaDatos.sql
+
+-- Creación Dim_time
+
+source /scripts/2.Carga-Dim_time.sql
+
+-- Creación Dim_store
+
+source /scripts/3.Carga-Dim_store.sql
+
+-- Creación Dim_category
+
+source /scripts/4.Carga-Dim_category.sql
+
+-- Creación Dim_subcategory
+
+source /scripts/5.Carga-Dim_subcategory.sql
+
+-- Creación Dim_product
+
+source /scripts/6.Carga-Dim_product.sql
+
+-- Creación Fact_Ventas
+
+source /scripts/7.Carga-Fact_ventas.sql
+
+-- Consulta # 1
+
+source /scripts/8.consulta.sql
+
+-- Consulta # 2
+
+source /scripts/9.Consulta-2.sql
---
+++ scripts/3.Carga-Dim_store.sql
@@ -0,0 +1,8 @@
+INSERT INTO adw_TD_datawh.dim_Store (
+    location_id,
+    Location_name
+)
+SELECT 
+BusinessEntityID,
+Name as Nombre
+FROM adw.Sales_Store;
